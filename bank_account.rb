a98b3017eb60b963f6278d9abb0a9c59ed55ec7e
Trace Helms <tracehelms@gmail.com>
2014-02-19 20:20:15 -0500

    Dojo progress

--- bank_account.rb
+++ bank_account.rb
@@ -1,5 +1,20 @@
+require "money"
+
 class BankAccount
 
-  # Fill me in...
+  attr_reader :balance
 
+  #Create a new BankAccount object with an
+  # optional parameter to set and initial_amount
+  def initialize(initial_amount = 0)
+    @balance = initial_amount.to_money
+  end
+
+  def add_funds(additional_amount)
+    @balance += additional_amount.to_money
+  end
+
+  def withdraw(amt)
+    @balance -= amt.to_money
+  end
 end
--- bank_account_spec.rb
+++ bank_account_spec.rb
@@ -2,7 +2,37 @@ require './bank_account.rb'
 
 describe BankAccount do
 
-  it 'first test' do
+  before(:each) do
+    @ba = BankAccount.new(500)
+  end
+
+  it 'should create a bank account' do
+    ba = BankAccount.new
+    expect(ba).to be_instance_of BankAccount
+  end
+
+  it 'should create a new bank account instance with amount' do
+    expect(@ba).to be_instance_of BankAccount
+    expect(@ba.balance).to eq(500)
+  end
+
+  it 'should have amount as a money object' do
+    amount = @ba.balance
+    expect(amount).to be_instance_of Money
+  end
+
+  it 'should respond to balance' do
+    expect(@ba).to respond_to(:balance)
+  end
+
+  it 'should add funds' do
+    @ba.add_funds(100)
+    expect(@ba.balance).to eq(600)
+  end
+
+  it 'should withdraw funds' do
+    @ba.withdraw(50)
+    expect(@ba.balance).to eq(450)
   end
 
 end
